99c4f19eb81ba384001577c7cd6ab0ee8c0d1435
mityamuch <74504728+mityamuch@users.noreply.github.com>
2021-12-10 05:17:34 +0300

    DialogHost preready

--- FirstMVVMProg/First.MvvmProg.Client/Controls/DialogHost.xaml.cs
+++ FirstMVVMProg/First.MvvmProg.Client/Controls/DialogHost.xaml.cs
@@ -22,5 +22,45 @@ namespace FIrstMVVMProg.Client.Controls
         {
             InitializeComponent();
         }
+        #region Properties
+        #region CornRadius
+        public static DependencyProperty CornRadiusProperty = DependencyProperty.Register(nameof(CornRadius), typeof(string), typeof(DialogHost), new UIPropertyMetadata("0,0,0,0"));
+        public string CornRadius
+        {
+            get { return (string)GetValue(CornRadiusProperty); }
+            set { SetValue(CornRadiusProperty, value); }
+        }
+        #endregion
+        #region Opacity
+        public static DependencyProperty OpacityBorderProperty = DependencyProperty.Register(nameof(OpacityBorder), typeof(double), typeof(DialogHost), new UIPropertyMetadata("0.4"));
+        public double OpacityBorder
+        {
+            get { return (double)GetValue(OpacityBorderProperty); }
+            set { SetValue(OpacityBorderProperty, value); }
+        }
+        #endregion
+        #region Command
+        public static DependencyProperty ClickCommandProperty = DependencyProperty.Register(nameof(ClickCommand), typeof(ICommand), typeof(DialogHost), new UIPropertyMetadata());
+        public ICommand ClickCommand
+        {
+            get { return (ICommand)GetValue(ClickCommandProperty); }
+            set { SetValue(ClickCommandProperty, value); }
+        }
+
+
+        #endregion
+
+        #endregion
+
+        public void OnClick(object sender, MouseButtonEventArgs e)
+        {
+            if (ClickCommand != null)
+            {
+                Point p = e.GetPosition(this);
+                ClickCommand.Execute(p);
+            }
+        }
+
+
     }
 }
